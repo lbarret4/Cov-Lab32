26d3970a32fc106bad68990caa6489fa8630dd63
lbarret4 <llewellynbar4@gmail.com>
2018-09-21 15:41:10 -0400

    added users router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import chirpsRouter from './chirps';
-
+import usersRouter from './users';
 let router = Router();
 
 router.use('/chirps', chirpsRouter);
+router.use('/users',usersRouter);
 
 export default router;
