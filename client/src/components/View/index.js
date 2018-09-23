d436f51fd66ebdaef1cd834ac0588a2995f9cc20
lbarret4 <llewellynbar4@gmail.com>
2018-09-23 06:10:09 -0400

    added chirpMEntions

--- client/src/components/View/index.js
+++ client/src/components/View/index.js
@@ -1,6 +1,6 @@
 import ChirpEdit from './ChirpEdit';
 import ChirpsFeed from './ChirpsFeed';
 import ChirpCard from './ChirpCard';
+import ChirpMentions from './ChirpMentions'
 
-
-export { ChirpCard, ChirpEdit, ChirpsFeed };
+export { ChirpCard, ChirpEdit, ChirpsFeed,ChirpMentions };
