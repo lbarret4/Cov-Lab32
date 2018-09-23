87a8df33d7344b9c251a5e30f5d99aad0ad48428
lbarret4 <llewellynbar4@gmail.com>
2018-09-23 05:52:44 -0400

    modified to return user's name and renamed column alias

--- chirpr-sp.sql
+++ chirpr-sp.sql
@@ -3,13 +3,15 @@ delimiter //
 CREATE PROCEDURE spUserMentions(userid int )
 BEGIN
    SELECT 
-        ch.id AS ChirpId,
-        ch.text AS ChirpText,
-        ch._created AS ChirpDate
-    FROM chirps ch
-    JOIN mentions mn ON mn.chirpid = ch.id
-    JOIN users us ON  mn.userid = us.id
-    WHERE mn.userid = userid;
+		ch.id AS id,
+        ch._created AS time,
+        us2.name AS user,
+        ch.text AS content        
+	FROM chirps ch
+	JOIN mentions mn ON mn.chirpid = ch.id
+	JOIN users us1 ON  mn.userid = us1.id
+	JOIN users us2 ON ch.userid =us2.id
+	WHERE mn.userid =userid;
 
 
 
